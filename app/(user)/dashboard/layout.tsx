f9bcbd73cec31bd016dd99e757a0c06551e97a4b
jessejuwe <jessejuwe@gmail.com>
2023-03-10 10:25:07 +0100

    new file

--- app/(user)/dashboard/layout.tsx
+++ app/(user)/dashboard/layout.tsx
@@ -3,9 +3,9 @@
 import React, { useEffect } from 'react';
 import { usePathname } from 'next/navigation';
 
-import { Providers } from '../providers';
-import Fonts from '../fonts/Fonts';
-import { NavBar, Footer } from '../../exports/exports';
+import { Providers } from '../../providers';
+import Fonts from '../../fonts/Fonts';
+import { NavBar, Footer } from '@/exports/exports';
 import '../../styles/globals.css';
 
 export default function RootLayout({
