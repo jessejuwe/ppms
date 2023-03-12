1d21620d9f374dbb3253e955f469d0c1f33cd10a
jessejuwe <jessejuwe@gmail.com>
2023-03-12 23:13:10 +0100

    Fifth commit

--- app/(main)/sign-up/page.tsx
+++ app/(main)/sign-up/page.tsx
@@ -1,4 +1,3 @@
-import { SignUpURL } from '@/helpers/urls';
 import { Register } from '../../../exports/exports';
 
 export const metadata = {
@@ -9,20 +8,10 @@ export const metadata = {
   description: 'PPMS sign up page',
 };
 
-// Data fetching - getServerSideProps()
-async function getUsers() {
-  const res = await fetch(`${SignUpURL}`, { cache: 'no-store' });
-  const data = await res.json();
-
-  return data;
-}
-
 export default async function SignUp() {
-  const users = await getUsers();
-
   return (
     <main>
-      <Register users={users} />
+      <Register />
     </main>
   );
 }
